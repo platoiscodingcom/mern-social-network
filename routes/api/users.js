d325dc0b005f090c29da3cf6d8ab21355b04990f
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2019-11-26 18:05:05 +0100

    implementing jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route     GET api/users
 // @desc      Register User
@@ -46,9 +48,21 @@ async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(password, salt);
   await user.save();
-  
-  // return jsonwebtoken
-  res.send('User registered');
+
+  const payload =  {
+    user: {
+      id: user.id
+    }
+  }
+
+  jwt.sign(
+    payload, 
+    config.get('jwtSecret'), 
+    { expiresIn: 360000 },
+    (err, token) => {
+      if(err) throw err;
+      res.json({token});
+    });
     
   }catch(err){
     console.error(err.message);
